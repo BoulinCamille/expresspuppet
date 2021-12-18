bc248502528b669720a1c0735ff782503aaac1cf
BoulinCamille <camille.boulin@outlook.fr>
2021-12-19 00:36:54 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+const functions = require('./test/functions')
+
+
+
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin);
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+})); 
+
+app.use(bodyParser.json() );
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname+'/index.html'));
+})
+
+
+app.post('/crawl', async (req, res) => {
+    const products = {};
+    const url = req.body.url;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const hrefs = await functions.getAllHrefs(page);
+    const uniqueHrefs = [... new Set(hrefs)];
+    uniqueHrefs.push(url);
+    let i = 1;
+    
+    while (uniqueHrefs.length > 0) {
+        const page2 = await browser.newPage();
+        try {
+            console.log('going to ' + uniqueHrefs[uniqueHrefs.length-1]);
+            await page2.goto(uniqueHrefs[uniqueHrefs.length-1]);
+        } catch(e) {
+            console.log(e);
+        }
+        
+        try {
+            dataObj = {}
+            dataObj['price'] = functions.getPrice(page, req.body.price);
+            dataObj['name'] = await page2.$eval(req.body.name, n => n.textContent);
+            dataObj['url'] = uniqueHrefs[uniqueHrefs.length-1];
+            products[i] = dataObj;
+            console.log("YESPRODUI");
+            i++;
+        } catch (e) {
+            console.log('nope');
+        }
+        uniqueHrefs.pop();
+        await page2.close();
+        console.log(uniqueHrefs.length)
+    }
+    await page.close();
+    await browser.close();
+    res.send(products);
+})
+
+
+app.post('/crawl-one', async (req, res) => {
+    datas = [];
+    for (let i = 0; i <=10; i++) {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(req.body.url);
+        const dataObj = await functions.scanOneUrl(page, req.body.price, req.body.name);
+        datas.push(dataObj);
+        await page.close();
+        await browser.close();
+    }
+    
+
+    res.send(datas);
+});
+
+app.listen(port, () => {
+    console.log("poney");
+});
---
+++ test/functions.js
@@ -0,0 +1,49 @@
+
+
+async function getAllHrefs(page) {
+    try {
+        let hrefs = await page.$$eval('a', (anchorEls) => anchorEls.map((a) => a.href));
+        return hrefs;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+async function getPrice(page, selector) {
+    let price = null;
+    try {
+        price = await page.$eval(selector, p => p.value);
+    } catch(e) {
+        console.log(e);
+    }
+    try {
+        price = await page.$eval(selector, p => p.textContent);
+    } catch(e) {
+        console.log(e);
+    }
+    
+    return price;
+}
+
+
+async function getName(page, selector) {
+    let name = null
+    try {
+        name = await page.$eval(selector, n => n.textContent);
+    } catch(e) {
+        console.log(e);
+    }
+    
+    return name;
+}
+
+async function scanOneUrl(page, priceSelector, nameSelector) {
+    dataObj = {};
+    dataObj['price'] = await getPrice(page, priceSelector);
+    dataObj['name'] = await getName(page, nameSelector);
+    return dataObj;
+}
+
+
+module.exports = { getAllHrefs, getPrice, getName, scanOneUrl }
