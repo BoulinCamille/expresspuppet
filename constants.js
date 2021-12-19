3ffe9136ebbae4dd414db506a2134f4af54cb731
BoulinCamille <camille.boulin@outlook.fr>
2021-12-20 00:08:05 +0100

    datadome cookie and script

---
+++ constants.js
@@ -0,0 +1,27 @@
+function blockedResources() {
+    const blockedResources = [
+        'quantserve',
+        'adzerk',
+        'doubleclick',
+        'adition',
+        'exelator',
+        'sharethrough',
+        'twitter',
+        'google-analytics',
+        'fontawesome',
+        'facebook',
+        'analytics',
+        'optimizely',
+        'clicktale',
+        'mixpanel',
+        'zedo',
+        'clicksor',
+        'tiqcdn',
+        'googlesyndication',
+      ];
+
+    return blockedResources;
+}
+
+
+module.exports = { blockedResources };
--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+const pup = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+// const randomUserAgent = require("random-useragent");
+const constants = require('./constants');
+pup.use(StealthPlugin);
+
+
 async function getAllHrefs(page) {
     try {
         let hrefs = await page.$$eval('a', (anchorEls) => anchorEls.map((a) => a.href));
@@ -42,11 +49,46 @@ async function getName(page, selector) {
     return name;
 }
 
-async function scanOneUrl(page, priceSelector, nameSelector) {
-    dataObj = {};
-    dataObj['price'] = await getPrice(page, priceSelector);
-    dataObj['name'] = await getName(page, nameSelector);
-    return dataObj;
+async function scanOneUrl(url, priceSelector, nameSelector) {
+    const browser = await createBrowser();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const blockedResources = constants.blockedResources();
+    console.log(blockedResources);
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+        if (request.url().endsWith('.png') || request.url().endsWith('.jpg'))
+            request.abort();
+        else if (blockedResources.some(resource => blockedResources.indexOf(resource) !== -1))
+            request.abort();
+        else
+            request.continue();
+      });
+    const cookies = await page.cookies();
+    // await page.deleteCookie(...cookies);
+    const datadomeCookieValue = cookies[0]['value'];
+
+    const scripts = await page.$$eval("script", scripts => scripts.map(script => script.textContent));
+    regex = /^var dd.+/
+    let script = "";
+    scripts.forEach(el => {
+        if (el.match(regex)) {
+            script = el;
+        }
+    });
+    script = script.replace("var dd=", "");
+    script = replaceAll(script, "'", '"');
+    script = JSON.parse(script);
+    const initialCid = script['cid'];
+    const hsh = script['hsh'];
+    const t = script['t'];
+    const host = script['host'];
+    const cid = datadomeCookieValue;
+    console.log(script);
+    // dataObj = {};
+    // dataObj['price'] = await getPrice(page, priceSelector);
+    // dataObj['name'] = await getName(page, nameSelector);
+    return page.content();
 }
 
 
@@ -63,4 +105,18 @@ function deleteCurrencyPrice(price) {
 }
 
 
-module.exports = { deleteCurrencyPrice, getAllHrefs, getPrice, getName, scanOneUrl }
+async function createBrowser() {
+    const browser = await pup.launch()
+    const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+    await browser.userAgent(UA);
+
+    return browser;
+}
+
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+
+module.exports = { createBrowser, deleteCurrencyPrice, getAllHrefs, getPrice, getName, scanOneUrl }
